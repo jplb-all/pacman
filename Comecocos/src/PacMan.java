0d9da459680717f6082c650bf114165acbde84cd
Jose Ramón Álvarez Padial <joseavi93@gmail.com>
2013-05-31 13:21:08 +0200

    Commit inicial PacMan

--- Comecocos/src/PacMan.java
+++ Comecocos/src/PacMan.java
@@ -1,12 +1,32 @@
 import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 
 public class PacMan extends Personaje{
 	
-	public PacMan(Laberinto l) {
+	private BufferedImage img;
+	private Point2D.Double ds;	// desplazamiento (x, y) para seleccionar el sprite del
+	                            // pacman que se tiene que diburar en un instante determinado.
+	
+	public PacMan(Laberinto l, String fichero) {
 		super(NombresPersonaje.PACMAN, l);
+		try {
+			ImageIO.read(PacMan.class.getResource(fichero));
+		} catch (IOException e) {}
 	}
 
+	public BufferedImage getImg() {
+		return img;
+	}
+	
+	public Point2D.Double getCoordenadasSprite() {
+		
+		return null;
+	}
+	
 	public void mover(double velocidad, Direccion nuevaDireccion){
 		// El parámetro nuevaDirección representa la dirección que tiene que tomar si fuese posible.
 		super.mover(velocidad);
