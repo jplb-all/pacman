70d3e6627d85ed4bce76913d67f7873ef3d3efb3
Jose Ramón Álvarez Padial <joseavi93@gmail.com>
2013-06-06 11:57:18 +0200

    Correcion de errores

--- Comecocos/src/PacMan.java
+++ Comecocos/src/PacMan.java
@@ -27,50 +27,32 @@ public class PacMan extends Personaje{
 		return null;
 	}
 	
+	
 	public void mover(double velocidad, Direccion nuevaDireccion){
 		// El parámetro nuevaDirección representa la dirección que tiene que tomar si fuese posible.
 		super.mover(velocidad);
 		Direccion d = getDireccion();
 		Point2D.Double p = getPosicion();
-		switch (d){
+		if(d != nuevaDireccion) 
+			switch (d){
 		case ARRIBA:
-			if(nuevaDireccion == Direccion.ABAJO){
+			if ((nuevaDireccion == Direccion.ABAJO) || ((int) p.y % 19 == 0 && !muro(nuevaDireccion))){
 				setDireccion(nuevaDireccion);
-			}
-			else if(nuevaDireccion != Direccion.ARRIBA && (int) p.y % 19 == 0){
-				if(!muro(nuevaDireccion)){
-					setDireccion(nuevaDireccion);
-				}
 			}
 			break;
 		case IZDA:
-			if(nuevaDireccion == Direccion.DERECHA){
+			if ((nuevaDireccion == Direccion.DERECHA) || ((int) p.x % 19 == 0 &&!muro(nuevaDireccion))){
 				setDireccion(nuevaDireccion);
-			}
-			else if(nuevaDireccion != Direccion.IZDA && (int) p.x % 19 == 0){
-				if(!muro(nuevaDireccion)){
-					setDireccion(nuevaDireccion);
-				}
 			}
 			break;
 		case ABAJO:
-			if(nuevaDireccion == Direccion.ARRIBA){
+			if ((nuevaDireccion == Direccion.ARRIBA) || ((int) p.y % 19 == 0 &&!muro(nuevaDireccion))){
 				setDireccion(nuevaDireccion);
-			}
-			else if(nuevaDireccion != Direccion.ABAJO && (int) p.y % 19 == 0){
-				if (!muro(nuevaDireccion)){
-					setDireccion(nuevaDireccion);
-				}
 			}
 			break;
 		case DERECHA:
-			if(nuevaDireccion == Direccion.IZDA){
+			if ((nuevaDireccion == Direccion.IZDA) || ((int) p.x % 19 == 0 &&!muro(nuevaDireccion))){
 				setDireccion(nuevaDireccion);
-			}
-			else if(nuevaDireccion != Direccion.DERECHA && (int) p.x % 19 == 0){
-				if(!muro(nuevaDireccion)){
-					setDireccion(nuevaDireccion);
-				}
 			}
 			
 			break;
